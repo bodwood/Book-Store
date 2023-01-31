e3477c06d8d2769628d4bfbffdc246f3aebb050f
bodwood <bodiewood@hotmail.com>
2023-01-31 08:26:39 -0800

    insert title above booklist

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import './index.css'
 //what we want to place in elements
 const BookList = () => {
   return (
+    <>
+    <h1>Best Sellers</h1>
     <section className='booklist'>
       {books.map((book, index) => {
         return <Book {...book} key={book.id} number={index} />
       })}
     </section>
+    </>
   )
 }
 
